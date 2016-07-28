6b541bcbde71bdd4160d8ce1aa7a40f9f4a29385
Dustin Parham <dr.parham@gmail.com>
2016-07-28 17:10:16 -0600

    updating migration command

--- app/Console/Commands/Migrate.php
+++ app/Console/Commands/Migrate.php
@@ -5,13 +5,14 @@ use Illuminate\Contracts\Console\Kernel;
 
 class Migrate extends Command
 {
-    protected $signature = 'weblog:migrate';
+    protected $signature = 'weblog:migrate {--f|force : Force application migrations to run before weblog migrations}';
     protected $description = 'Run migrations for Laravel Weblog.';
-
     public function handle()
     {
-        $this->comment('Run application migrations, just to make sure the default migrations have been run.');
-        $this->call('migrate');
+        if($this->option('force')){
+            $this->comment('Run application migrations, just to make sure the default migrations have been run.');
+            $this->call('migrate');
+        }
         $this->comment('Run Laravel Weblog migrations.');
         $this->call('migrate', [
             '--path' => 'vendor/genealabs/laravel-weblog/database/migrations',
