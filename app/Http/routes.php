20da517a28287af228b4e9c48531a394f1f4c124
Dustin Parham <dr.parham@gmail.com>
2016-07-28 15:01:56 -0600

    change config path for route definitions

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,9 +5,9 @@ use GeneaLabs\LaravelWeblog\Http\Controllers\Posts;
 use GeneaLabs\LaravelWeblog\Http\Controllers\Rss;
 use GeneaLabs\LaravelWeblog\Http\Controllers\Sitemap;
 
-Route::get(config('vendor.genealabs.laravel-weblog.blog-route-name'), Posts::class.'@index');
-Route::get(config('vendor.genealabs.laravel-weblog.rss-route-name'), Rss::class.'@index');
-Route::get(config('vendor.genealabs.laravel-weblog.sitemap-route-name'), Sitemap::class.'@index');
+Route::get(config('laravel-weblog.blog-route-name'), Posts::class.'@index');
+Route::get(config('laravel-weblog.rss-route-name'), Rss::class.'@index');
+Route::get(config('laravel-weblog.sitemap-route-name'), Sitemap::class.'@index');
 Route::resource('posts', Posts::class);
 
 Route::group(['prefix' => 'genealabs/laravel-weblog'], function () {
